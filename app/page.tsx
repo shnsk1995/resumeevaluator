eea8b0349fd7cf0323a2c2381686cbf2a0c9a3bc
shnsk1995 <119891144+shnsk1995@users.noreply.github.com>
2025-12-21 16:33:07 -0600

    AI UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
    return (
-    <div className="p-5 border bg-red-500 min-h-screen rounded">
+    <div className="p-5 border bg-yellow-500 min-h-screen rounded-3xl shadow-lg">
       {/* Page */}
-      <div className="mx-auto border border-orange-500 rounded  p-2 border-10 bg-yellow-100 h-[95dvh]">
+      <div className="mx-auto border border-pink-500 rounded-3xl  p-2 border-10 bg-yellow-100 h-[95dvh] shadow-xl shadow-pink-500/50 ">
         
         {/* Header */}
-        <div className="border border-green-500 border-5 p-2 rounded text-center">
+        <div className="border border-green-500 border-5 p-2 rounded-xl text-center shadow-lg shadow-green-500/50 mt-2 bg-red-200">
           Resume Evaluator
         </div>
 
         {/* Messages */}
-        <div className="border border-green-500 border-5 p-2 mt-2 space-y-2 h-[78dvh]">
-          <div className="border p-2 rounded w-3xl ml-auto">
+        <div className="border border-green-500 border-5 bg-amber-100 p-2 mt-2 space-y-2 h-[70dvh] lg:h-[70dvh] shadow-lg shadow-green-500/50 rounded-xl">
+          <div className="border border-blue-500 border-3 p-2 rounded-lg bg-blue-100 w-xl md:w-2xl lg:w-3xl ml-auto">
             Hello!
           </div>
-          <div className="border p-2 rounded w-3xl mr-auto">
+          <div className="border border-gray-500 border-3 bg-gray-100 p-2 rounded-lg w-xl md:w-2xl lg:w-3xl mr-auto">
             Hi there!
           </div>
         </div>
 
         {/* Input */}
-        <div className="flex gap-2 border border-dashed p-2 mt-2">
+        <div className="flex gap-2 border border-green-500 border-5 shadow-lg shadow-green-500/50 rounded-xl p-2 mt-2">
           <input
-            className="flex-1 border p-2 rounded"
+            className="flex-1 border p-2 rounded-xl border-gray-500 border-2 shadow-lg text-pink-900"
             placeholder="Type message..."
           />
-          <button className="border px-4 rounded">
+          <button className="border px-4 rounded-xl border-gray-500 border-2 shadow-lg hover:bg-pink-500 hover:text-white">
             Send
           </button>
         </div>
