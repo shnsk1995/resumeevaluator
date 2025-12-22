4c70145f626a9a22835960b3087e4d4726063fbb
shnsk1995 <119891144+shnsk1995@users.noreply.github.com>
2025-12-21 21:26:51 -0600

    AI UI

--- app/page.tsx
+++ app/page.tsx
@@ -118,8 +118,8 @@ export default function Home() {
         </div>
 
         {/* Input */}
-        <div className="flex flex-col gap-2 border border-green-500 border-5 shadow-lg shadow-green-500/50 rounded-xl p-2 mt-2 items-center">
-        <form className="flex flex-1 gap-4 items-center">
+        <div className="flex sm:flex-col gap-2 border border-green-500 border-5 shadow-lg shadow-green-500/50 rounded-xl p-2 mt-2 items-center">
+        <form className="gap-4 items-center">
 
         <input
             className="border p-2 rounded-xl border-gray-500 border-2 shadow-lg text-pink-900"
