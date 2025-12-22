88003b8eb1c55d3c31b61cbb114205a664b5178f
shnsk1995 <119891144+shnsk1995@users.noreply.github.com>
2025-12-21 20:45:57 -0600

    AI UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,131 @@
+"use client";
 import Image from "next/image";
+import { useReducer, useState } from "react";
+
+
+function uuid(){
+  return Math.random().toString(36).slice(2) + Date.now().toString(36);
+}
+
+const initialState = {
+  messages : [],
+};
+
+function reducer(state, action){
+  switch(action.type){
+    case "ADD_MESSAGES":
+      console.log("Happy!")
+      return { ...state, messages: [...state.messages, ...action.payload]}
+  }
+
+}
+
+function UserMessage({msg}){
+  return(
+  <div className="border border-blue-500 border-3 p-2 rounded-lg bg-blue-100 ml-auto text-blue-900">
+    <p className="border rounded-lg text-center mb-4">User</p>
+    {msg}
+  </div>);
+}
+
+function AssistantMessage({msg}){
+  return(
+    <div className="border border-gray-500 border-3 bg-gray-100 p-2 rounded-lg mr-auto text-gray-900">
+      <p className="border rounded-lg text-center mb-4">Assistant</p>
+      {msg}
+  </div>);
+}
+
+
 
 export default function Home() {
+
+  const [state,dispatch] = useReducer(reducer,initialState)
+  const [text,setText] = useState("")
+  const [roles,setRoles] = useState("")
+
+
+  function newUserMessage(userMessage){
+
+    const userMsgId = uuid();
+    const assistantMsgId = uuid();
+
+    console.log(userMsgId);
+    console.log(assistantMsgId)
+
+    dispatch({
+
+      type : "ADD_MESSAGES",
+      payload : [{
+        id: userMsgId,
+        role : "user",
+        content: userMessage
+      },{
+        id : assistantMsgId,
+        role : "assistant",
+        content: "Received your message. WIll reply soon!"
+      }]
+    });
+
+  }
+
+  function OnSubmit(e){
+    e.preventDefault();
+    const userMessage = text.trim();
+    if(!userMessage) return;
+    setText("");
+    newUserMessage(userMessage);
+  }
+
+
    return (
     <div className="p-5 border bg-yellow-500 min-h-screen rounded-3xl shadow-lg">
       {/* Page */}
       <div className="mx-auto border border-pink-500 rounded-3xl  p-2 border-10 bg-yellow-100 h-[95dvh] shadow-xl shadow-pink-500/50 ">
         
         {/* Header */}
-        <div className="border border-green-500 border-5 p-2 rounded-xl text-center shadow-lg shadow-green-500/50 mt-2 bg-red-200">
+        <div className="border border-green-500 border-5 p-2 rounded-xl text-center shadow-lg shadow-green-500/50 mt-2 bg-red-200 text-pink-900">
           Resume Evaluator
         </div>
 
         {/* Messages */}
         <div className="border border-green-500 border-5 bg-amber-100 p-2 mt-2 space-y-2 h-[70dvh] lg:h-[70dvh] shadow-lg shadow-green-500/50 rounded-xl">
-          <div className="border border-blue-500 border-3 p-2 rounded-lg bg-blue-100 ml-auto">
-            Hello!
-          </div>
-          <div className="border border-gray-500 border-3 bg-gray-100 p-2 rounded-lg mr-auto">
-            Hi there!
-          </div>
+          
+        {state.messages.map(m => (
+              m.role==="user" ? 
+              <UserMessage  key={m.id} msg={m.content} /> :
+              <AssistantMessage  key={m.id} msg={m.content} />
+            ))
+}
+          
+          
         </div>
 
         {/* Input */}
-        <div className="flex gap-2 border border-green-500 border-5 shadow-lg shadow-green-500/50 rounded-xl p-2 mt-2">
-          <input
-            className="flex-1 border p-2 rounded-xl border-gray-500 border-2 shadow-lg text-pink-900"
+        <div className="flex flex-col gap-2 border border-green-500 border-5 shadow-lg shadow-green-500/50 rounded-xl p-2 mt-2 items-center">
+        <form className="flex flex-1 gap-4 w-80 items-center">
+
+        <input
+            className="border p-2 rounded-xl border-gray-500 border-2 shadow-lg text-pink-900"
             placeholder="Type message..."
+            onSubmit={OnSubmit}
+            value={text}
+            onChange={(e)=> setText(e.target.value)}
           />
-          <button className="border px-4 rounded-xl border-gray-500 border-2 shadow-lg hover:bg-pink-500 hover:text-white">
+          <input
+            className="border p-2 rounded-xl border-gray-500 border-2 shadow-lg text-pink-900"
+            placeholder="Targeting roles...."
+            onSubmit={OnSubmit}
+            value={roles}
+            onChange={(e)=> setRoles(e.target.value)}
+          />
+          <button className="border p-2 rounded-xl border-gray-500 border-2 shadow-lg hover:bg-pink-500 hover:text-white text-pink-900" onClick={OnSubmit}>
             Send
           </button>
+
+
+        </form>
+        
         </div>
       </div>
     </div>
